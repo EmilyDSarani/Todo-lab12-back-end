f99fd32cd4877549ea3b0796a1b4285abf57ae7c
EmilyDSarani <emily.sarani@gmail.com>
2021-10-11 15:42:30 -0500

    got the initial setup

---
+++ data/todo.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    chore: 'Take Out Trash',
+    completed: false,
+  },
+  {
+    chore: 'Wash Car',
+    completed: false,
+  },
+  {
+    chore: 'Do Dishes',
+    completed: false,
+  },
+  {
+    chore: 'Buy Snacks',
+    completed: false,
+  }
+];
